57d4283240e1bb8109238883aabb33aa17ebffcf
mattnorris1990 <mattnorris1990@gmail.com>
2022-05-19 15:00:25 +0100

    added an h2

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -4,6 +4,7 @@ const TitleBar = ({handleSelectChange, genres}) =>{
   return (
     <div>
       <h1>MaFajs Hits Parade</h1>
+      <h2>CAM SUXXX</h2>
       <select onChange={handleSelectChange}>
         {genres.map(genre => {
           return <option key={genre.name} value={genre.url}>{genre.name}</option>
