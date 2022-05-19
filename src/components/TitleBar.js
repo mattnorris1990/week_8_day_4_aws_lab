a6413273c637f656e80a1173f1a041a823d0ab19
mattnorris1990 <mattnorris1990@gmail.com>
2022-05-19 14:46:57 +0100

    updated title

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -4,7 +4,6 @@ const TitleBar = ({handleSelectChange, genres}) =>{
   return (
     <div>
       <h1>MaFaj's Hits Parade</h1>
-      <h2>Cam sux</h2>
       <select onChange={handleSelectChange}>
         {genres.map(genre => {
           return <option key={genre.name} value={genre.url}>{genre.name}</option>
